26df96c09c762edb457b4c2b31e30f191e77d175
kymani <kymanist00@gmail.com>
2025-07-30 23:44:07 -0700

    cleanup js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 // FOR PARALLAX SCROLLING
-
+ 
 window.addEventListener("scroll", () => {
   const img = document.getElementById("parallax-img");
   const container = document.querySelector(".section4-container");
@@ -8,7 +8,6 @@ window.addEventListener("scroll", () => {
   const containerRect = container.getBoundingClientRect();
   const speed = 0.1;
 
-  // Only apply effect when the section is in view
   if (containerRect.top < window.innerHeight && containerRect.bottom > 0) {
     const offset = containerRect.top * speed;
     img.style.transform = `translateY(${offset}px)`;
@@ -25,21 +24,20 @@ document.addEventListener("DOMContentLoaded", () => {
     const previous = current;
     current = (current + 1) % images.length;
 
-    // Fade out the current
+
     images[previous].classList.remove("fade-in");
     images[previous].classList.add("fade-out");
 
-    // Fade in the next
+    
     images[current].classList.remove("fade-out");
     images[current].classList.add("fade-in");
 
-    // Clean up after transition
+  
     setTimeout(() => {
       images[previous].classList.remove("fade-out");
-    }, 3000); // Matches your CSS transition duration
+    }, 3000); 
   }
 
-  // Initialize
   images[current].classList.add("fade-in");
 
   setInterval(crossfadeNext, 11000);
